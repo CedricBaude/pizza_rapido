796d8df02b3063647cce3ac38f93c3e62a712ffb
unknown <baude.cedric.perso@gmail.com>
2022-08-19 10:36:27 +0200

    add bdd access 2

--- settings/config.php
+++ settings/config.php
@@ -1,9 +1,9 @@
 <?php
 // Informations d'identification
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'pizzeria');
+define('DB_SERVER', 'remotemysql.com:3306');
+define('DB_USERNAME', 'KvbV9Uv8qY');
+define('DB_PASSWORD', 'AzU583ZUYx');
+define('DB_NAME', 'KvbV9Uv8qY');
 
 // Connexion à la base de données MySQL 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
